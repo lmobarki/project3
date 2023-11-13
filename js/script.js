d76e4f3e3ae7e2b3e9c6c8656d5199c7d76ed04b
lmobarki <143443056+lmobarki@users.noreply.github.com>
2023-11-13 10:57:28 -0600

    map

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-function init(){
+<div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
 
-  }
+  
 
