ff97ebc0061466673439cc1c0a0ea5a64471bb9b
lmobarki <143443056+lmobarki@users.noreply.github.com>
2023-11-14 19:41:09 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function init(){
 	//alert('it works');
 	var el = document.getElementById('canvas');
-	var myLocation = new google.maps.LatLng(41.8358, -87.6270);
+	var myLocation = new google.maps.LatLng(41.835117, -87.627130);
 	var mapOptions = {
 		center: myLocation,
 		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
 	};
 
 	var myMap = new google.maps.Map(el, mapOptions);
