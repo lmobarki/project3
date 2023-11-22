900b7f40bff1fcb199f150fbc954d192eef9bff6
lmobarki <143443056+lmobarki@users.noreply.github.com>
2023-11-22 10:04:18 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ function init(){
 	var mapOptions = {
 		center: myLocation,
 		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
 	};
 
 	var myMap = new google.maps.Map(el, mapOptions);
