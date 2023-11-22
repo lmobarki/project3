f5041e85ac1a2372eed3b1b69646c3a50be6fbc2
lmobarki <143443056+lmobarki@users.noreply.github.com>
2023-11-22 10:36:59 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function init(){
 	var myLocation = new google.maps.LatLng(41.876, -87.628);
 	var mapOptions = {
 		center: myLocation,
-		zoom: 18,
+		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.HYBRID,
 	};
 
